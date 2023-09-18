87126514be6430e1ac5dd46525ea1f5961c49bae
Rafael Bernardo <rafamaxber@gmail.com>
2023-09-17 22:43:40 -0300

    refac: remove config, because prisma takecare automatically

--- src/database/prisma/prisma.service.ts
+++ src/database/prisma/prisma.service.ts
@@ -1,12 +1,5 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
-let count = 0;
-
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
-  async onModuleInit() {
-    console.log('PrismaService', ++count);
-    await this.$connect();
-  }
-}
+export class PrismaService extends PrismaClient {}
